18c438fec034c342952f66839c003a94f95f6ed6
tinghong123 <974293797@qq.com>
2015-04-15 21:52:24 +0500

    test2-1
    Signed-off-by: tinghong123 <974293797@qq.com>

---
+++ test2-1.py
@@ -0,0 +1,16 @@
+@@ -8,9 +8,15 @@ 
+############ДњТы################# 
+import math 
+p,k,T,M,NA,n,m,R=1.013e5,1.38e-23,300.15,32e-3,6.022e23,2.45e25,5.31e-26,8.31 
+n=p/(k*T) 
+print 'n=p/(k*T)=',n 
+n=M/NA 
+print 'n=M/NA=',n 
+rho=n*m 
+print 'rho=n*m=',rho 
+v=math.sqrt(3*R*T/M) 
+print 'v=math.sqrt(3*R*T/M)=',math.sqrt(v**2) 
+epsilonk=3/2*k*T 
+print 'epsilonk=3/2*k*T=',epsilonk 
+epsilon=m/M*5/2*R*T 
+print 'epsilon=m/M*5/2*R*T=',epsilon 
