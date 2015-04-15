dedd4e76944abfa31600a4628b3860e55ec12959
tinghong123 <974293797@qq.com>
2015-04-15 22:10:29 +0500

    Signed-off-by: tinghong123 <974293797@qq.com>

--- test2-1.py
+++ test2-1.py
@@ -1,25 +1,18 @@
- #-*- coding:utf-8 -*-
-
-######??????############
-#??:??
-#??:1403050219
-#??:??14-2?
-
-###########??###############
-
-
-###########??###############
-import math 
-p,k,T,M,NA,n,m,R=1.013e5,1.38e-23,300.15,32e-3,6.022e23,2.45e25,5.31e-26,8.31 
-n=p/(k*T) 
-print 'n=p/(k*T)=',n 
-n=M/NA 
-print 'n=M/NA=',n 
-rho=n*m 
-print 'rho=n*m=',rho 
-v=math.sqrt(3*R*T/M) 
-print 'v=math.sqrt(3*R*T/M)=',math.sqrt(v**2) 
-epsilonk=3/2*k*T 
-print 'epsilonk=3/2*k*T=',epsilonk 
-epsilon=m/M*5/2*R*T 
-print 'epsilon=m/M*5/2*R*T=',epsilon 
+@@ -0,0 +1,17 @@ 
++#-*- coding:utf-8 -*- 
++ 
++######??????############ 
++#??:?? 
++#??:1403050219 
++#??:??14-2? 
+ + 
++###########??############### 
++??17.32 
+ +###########??############### 
+ +import math 
+ +def main() 
+ +    a, b= input("???2????a,b: ") 
+ +	discRoot = math.atan(b/a) 
+ +	x=i    #i??????????? 
+ +	c=90-i #c????? 
+ +	print"????C=" .C 
